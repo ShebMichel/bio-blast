8d693740150f2d49a7400451e120d3ca42ac9a49
ShebMichel <michel.nzikou@alumni.uleth.ca>
2024-02-16 01:54:11 +0000

    added bio-blast.sh

---
+++ bio-blast.sh
@@ -0,0 +1,100 @@
+  659  git config --list
+  660  git config user.email="ShebMichel@github.com"
+  661  git push -u origin main
+  662  git log
+  663  git push -f origin main
+  664  git config user.email="45086441+ShebMichel@users.noreply.github.com"
+  665  git push -f origin main
+  666  cd ..
+  667  ls
+  668  mkdir test2
+  669  cd test2
+  670  git init
+  671  git remote add origin https://github.com/ShebMichel/test2.git
+  672  git branch -M main
+  673  git push -u origin main
+  674  history
+  675  git remote add origin https://github.com/ShebMichel/test2.git
+  676  git branch -M main
+  677  touch test1.txt
+  678  nano test1.txt 
+  679  git add test1.txt 
+  680  git commit -m "initial commit"
+  681  git push -u origin main
+  682  git config list
+  683  git config --list
+  684  git config user.email="45086441+ShebMichel@users.noreply.github.com"
+  685  git push -u origin main
+  686  cd..
+  687  cd .
+  688  cd ..
+  689  cd planets/
+  690  git push -u origin main
+  691  Preference would be march 25 for me. However, if the majority vote is for the 18, I am okay as well.
+  692  git pull
+  693  nano pluto.txt
+  694  git add pluto.txt 
+  695  git commit -m "added pluto.txt"
+  696  git push -u origin main
+  697  git push remote -u origin main
+  698  git push
+  699  git push -u origin main
+  700  git pull
+  701  git status
+  702  git push
+  703  git remote -v
+  704  git pull-rebase
+  705  git  --help
+  706  git push
+  707  ls
+  708  rm puppets.token 
+  709  ls
+  710  git status
+  711  git commit -a
+  712  git commit -a "deleted puppet"
+  713  git commit -m "deleted puppet"
+  714  git add 
+  715  git add .
+  716  git commit -m "deleted puppet"
+  717  git push
+  718  git branch --help
+  719  git branch --all
+  720  git branch -M main
+  721  git push -u origin main
+  722  git pull
+  723  ls
+  724  cd bio-blast/
+  725  ls
+  726  head -n 11
+  727  head -n 11 mouse.1.protein.faa > mm-first.fa
+  728  makeblastdb -in zebrafish.1.protein.faa -dbtype prot
+  729  blastp -query mm-first.fa -db zebrafish.1.protein.faa
+  730  blastp -query mm-first.fa -db zebrafish.1.protein.faa -out mm-first.x.zebrafish.txt
+  731  head -498 mouse.1.protein.faa > mm-second.fa
+  732  blastp -query mm-second.fa -db zebrafish.1.protein.faa -out mm-second.x.zebrafish.txt
+  733  blastp -query mm-second.fa -db zebrafish.1.protein.faa -out mm-second.x.zebrafish.tsv -outfmt 6
+  734  Rscript blastviz.R
+  735  jupyter notebook --ip=146.118.64.109
+  736  history
+  737  history tail -n 20
+  738  history | tail -n 20
+  739  history | bio-blash.sh
+  740  history | tail -n bio-blast.sh
+  741  mkdir bio-blast.sh
+  742  history | tail -n bio-blast.sh
+  743  history | tail -n 20 bio-blast.sh
+  744  rm -r bio-blast.sh/
+  745  history | tail -n 20 bio-blast.sh
+  746  nano bio-blast.sh
+  747  history | tail -n 20 bio-blast.sh
+  748  nano bio-blast.sh
+  749  history > tail -n 20 bio-blast.sh
+  750  nano bio-blast.sh
+  751  ls
+  752  rm -f bio-blast.sh 
+  753  ls
+  754  history > tail -n 20 > bio-blast.sh
+  755  nano bio-blast.sh
+  756  history | tail -n 20 > bio-blast.sh
+  757  nano bio-blast.sh
+  758  history | tail -n 100 > bio-blast.sh
